2bef0e0c682ca10fc32ee569ca3a9632bb1ca826
Karpenya <karpenyalexa1993@gmail.com>
2022-08-14 06:59:23 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите размер массива ");
+int countmassive = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[countmassive];
+Random rand = new Random();
+int[] numer= new int[] {} ;
+int maxvalue = 0;
+int minvalue = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = rand.Next(-100, 100);
+    Console.WriteLine(numbers[i]);
+}
+int s = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    s += numbers[i];
+}
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (maxvalue < numbers[i])
+    {
+        maxvalue = numbers[i];
+    }
+    if (minvalue > numbers[i])
+    {
+        minvalue = numbers[i];
+    }
+ 
+}
+
+Console.WriteLine($"Сумма всех чисел в массиве {s}");
+float b = s / (float)countmassive;
+Console.WriteLine($"Средне значение всех чисел в массиве {b}");
+Console.WriteLine($"Наибольшее значение в массиве {maxvalue}");
+Console.WriteLine($"Наименьшее значение в массиве {minvalue}");
